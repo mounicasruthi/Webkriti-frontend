f827418e9bee6944333b9c5cac20b70ea1c2374d
mounicasruthi <mounicasruthi30@gmail.com>
2021-07-08 12:12:24 +0530

    modified posts

--- profile/script.js
+++ profile/script.js
@@ -48,26 +48,11 @@ const createPosts = array => {
   container.innerHTML = "";
 
   array.forEach(cardObj => {
+    const { content, image } = cardObj1;
     
-
-    if(image) {
-      const { content, image } = cardObj;
-    const id = cardObj.postId;
-
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.id = id;
-    
-    const insideHtml = `<div class="card-header"><div class="left-section"><span class="dot"></span><div class="name">{username}</div></div><div class="right-section"><div class="like">{like-count}
-    </div><div class="icon"><img src="../assets/svg/likebutton.svg"></div> </div></div><div class="card-body"><p class="card-caption">${content}</p><img class="post-img" src="${image}" alt="postImg"/></div>`; //add username and like-count
-    
-    card.innerHTML = insideHtml;
-
-    container.appendChild(card);
-    }
-    else {
-      const { content } = cardObj;
-    const id = cardObj.postId;
+    if(!image){
+      const { content } = cardObj2;
+      const id = cardObj2.postId;
 
     const card = document.createElement("div");
     card.classList.add("card");
@@ -80,6 +65,46 @@ const createPosts = array => {
 
     container.appendChild(card);
     }
+    else{
+      const id = cardObj1.postId;
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.id = id;
+    
+    const insideHtml = `<div class="card-header"><div class="left-section"><span class="dot"></span><div class="name">{username}</div></div><div class="right-section"><div class="like">{like-count}
+    </div><div class="icon"><img src="../assets/svg/likebutton.svg"></div> </div></div><div class="card-body"><p class="card-caption">${content}</p><img class="post-img" src="${image}" alt="postImg"/></div>`; //add username and like-count
+    card.innerHTML = insideHtml;
+
+    container.appendChild(card);  
+    }
+
+    // if(image) {
+    //   const { content, image } = cardObj;
+    // const id = cardObj.postId;
+
+    // const card = document.createElement("div");
+    // card.classList.add("card");
+    // card.id = id;
+    
+    // const insideHtml = `<div class="card-header"><div class="left-section"><span class="dot"></span><div class="name">{username}</div></div><div class="right-section"><div class="like">{like-count}
+    // </div><div class="icon"><img src="../assets/svg/likebutton.svg"></div> </div></div><div class="card-body"><p class="card-caption">${content}</p><img class="post-img" src="${image}" alt="postImg"/></div>`; //add username and like-count
+    // }
+    // else {
+    //   const { content } = cardObj;
+    // const id = cardObj.postId;
+
+    // const card = document.createElement("div");
+    // card.classList.add("card");
+    // card.id = id;
+    //   const insideHtml = `<div class="card-header"><div class="left-section"><span class="dot"></span><div class="name">{username}</div></div><div class="right-section"><div class="like">{like-count}
+    // </div><div class="icon"><img src="../assets/svg/likebutton.svg"></div> </div></div><div class="card-body"><p class="card-caption">${content}</p></div>`; //add username and like-count
+
+    
+    // card.innerHTML = insideHtml;
+
+    // container.appendChild(card);
+    // }
 
   });
 };
