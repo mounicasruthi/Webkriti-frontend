385a577668135e4ca42294383fd79fa7824b8ec9
mounicasruthi <mounicasruthi30@gmail.com>
2021-07-08 18:52:01 +0530

    updated edit feature

--- profile/script.js
+++ profile/script.js
@@ -63,7 +63,7 @@ const createPosts = (array) => {
       card.id = id;
 
       const insideHtml = `<div class="card-header"><div class="left-section"><span class="dot"></span><div class="name">${name}</div></div><div class="right-section"><div class="like">{like-count}
-    </div><div class="icon"><img src="../assets/svg/likebutton.svg"></div> </div></div><div class="card-body"><p class="card-caption">${content}</p><img class="post-img" src="${image}" alt="" style={ { display: image ? 'block' : 'none' } }  /></div>`; //add username and like-count
+    </div><div class="icon"><img src="../assets/svg/likebutton.svg"></div><div class="editicon"><img src="../assets/svg/editbutton.svg"></div> </div></div><div class="card-body"><p class="card-caption">${content}</p><img class="post-img" src="${image}" alt="" style={ { display: image ? 'block' : 'none' } }  /></div>`; //add username and like-count
       card.innerHTML = insideHtml;
 
       container.appendChild(card);
