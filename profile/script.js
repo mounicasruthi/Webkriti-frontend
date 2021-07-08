bef2606501c578ddc0b65bd15f27867cbdf378a5
mounicasruthi <mounicasruthi30@gmail.com>
2021-07-08 18:44:18 +0530

    added edit button

--- profile/script.js
+++ profile/script.js
@@ -52,37 +52,25 @@ profileButton.addEventListener("click", () => {
 const createPosts = (array) => {
   container.innerHTML = "";
 
-  array.forEach((cardObj1, cardObj2) => {
-    const { content, image } = cardObj1;
-    cardObj1.content = cardObj2;
-
-    if (!image) {
-      const id = cardObj2.postId;
-
-      const card = document.createElement("div");
-      card.classList.add("card");
-      card.id = id;
-      const insideHtml = `<div class="card-header"><div class="left-section"><span class="dot"></span><div class="name">{username}</div></div><div class="right-section"><div class="like">{like-count}
-    </div><div class="icon"><img src="../assets/svg/likebutton.svg"></div><div class="editicon"><img src="../assets/svg/editbutton.svg"></div> </div></div><div class="card-body"><p class="card-caption">${content}</p></div>`; //add username and like-count
-
-      card.innerHTML = insideHtml;
-
-      container.appendChild(card);
-    } else {
-      const id = cardObj1.postId;
+  array.forEach((cardObj) => {
+    const { name, content, image } = cardObj;
+    console.log(cardObj);
+      
+      const id = cardObj.postId;
 
       const card = document.createElement("div");
       card.classList.add("card");
       card.id = id;
 
-      const insideHtml = `<div class="card-header"><div class="left-section"><span class="dot"></span><div class="name">{username}</div></div><div class="right-section"><div class="like">{like-count}
-    </div><div class="icon"><img src="../assets/svg/likebutton.svg"></div> </div></div><div class="card-body"><p class="card-caption">${content}</p><img class="post-img" src="${image}" alt="postImg"/></div>`; //add username and like-count
+      const insideHtml = `<div class="card-header"><div class="left-section"><span class="dot"></span><div class="name">${name}</div></div><div class="right-section"><div class="like">{like-count}
+    </div><div class="icon"><img src="../assets/svg/likebutton.svg"></div> </div></div><div class="card-body"><p class="card-caption">${content}</p><img class="post-img" src="${image}" alt="" style={ { display: image ? 'block' : 'none' } }  /></div>`; //add username and like-count
       card.innerHTML = insideHtml;
 
       container.appendChild(card);
-    }
+    
   });
 };
+
 
 const body = document.querySelector("body");
 
