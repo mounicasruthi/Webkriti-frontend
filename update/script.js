038fd9aae718a8f5c38407d499aab9d2ff229f77
mounicasruthi <mounicasruthi30@gmail.com>
2021-07-09 12:03:23 +0530

    modified update pages

--- update/script.js
+++ update/script.js
@@ -1,12 +1,11 @@
 const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get("postId");
 const updateButton = document.querySelector(".submit-button");
-const updatePostButton = document.querySelector(".createbtn");
 const homeButton = document.querySelector(".home");
 const profileButton = document.querySelector(".profile");
 const logout = document.querySelector(".logOut");
 
-const apiUrl = "https://aasf-final-project-backend.herokuapp.com";
+const apiUrl = "https://connectup-backend.herokuapp.com";
 
 const token = localStorage.getItem("jwt");
 
@@ -28,40 +27,62 @@ logout.addEventListener("click", () => {
     location.href = "/profile/profile.html";
   });
   
-
-updateButton.addEventListener("click", () => {
-  const content = document.querySelector(".post-content").value;
-  const image = document.querySelector(".post-image").value;
-
-  if (token) {
-    fetch(`${apiUrl}/note/update/${postId}`, {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const textInput = document.querySelector(".text-content");
+    const fileInput = document.querySelector(".image-content-input");
+    const formData = new FormData();
+    formData.append("content" ,textInput.value);
+    formData.append("image" ,fileInput.files[0]);
+    fetch(`${apiUrl}/posts/update/${postId}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
         authorization: token,
       },
-      body: JSON.stringify({ content, image }),
+      body: formData
+      
     })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.message) {
-          location.href = "/pages/dashboard/dashboard.html";
-        }
-      })
-      .catch((err) => {
-        alert("Could not edit note. Please try again.");
-        console.log(err);
-      });
-  }
-});
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .then((data) => {
+      if (data.message) {
+        location.href = "/profile/profile.html";
+      }
+    })
+    .catch((err) => {
+      alert("Could not update post. Please try again.");
+      console.log(err);
+    });
+  })
+  
 
+// updateButton.addEventListener("click", () => {
+//   const content = document.querySelector(".post-content").value;
+//   const image = document.querySelector(".post-image").value;
 
-
-
-
-
-
-
+//   if (token) {
+//     fetch(`${apiUrl}/posts/update/${postId}`, {
+//       method: "PUT",
+//       headers: {
+//         "Content-Type": "application/json",
+//         authorization: token,
+//       },
+//       body: JSON.stringify({ content, image }),
+//     })
+//       .then((res) => res.json())
+//       .then((data) => {
+//         if (data.message) {
+//           location.href = "/profile/profile.html";
+//         }
+//       })
+//       .catch((err) => {
+//         alert("Could not update post. Please try again.");
+//         console.log(err);
+//       });
+//   }
+// });
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
